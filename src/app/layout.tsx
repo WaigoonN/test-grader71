6238e054b46896d236775823f7ec3e12b1e55600
WaigoonN <cuswaigoon@gmail.com>
2024-08-23 01:28:46 +0700

    Added Tenz Tour

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 export const metadata = {
-  title: 'grader71',
+  title: 'Tenz Tour',
   description: '',
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,15 @@ import '@/styles/global.css'
 const Home = () => {
     return (
         <div className='width_normal'>
-            <Header selected = "Home" username = "PakinDioxide"/>
-            <p className="topic">Grader 71</p>
-            <p style={{fontSize: "1.5em"}}> Lorem ipsum <span className="text_highlight">dolor sit amet </span>consectetur adipisicing elit. <span className="text_highlight">Perferendis nemo </span>quam cupiditate officiis ipsam expedita <span className="text_highlight">accusantium at veniam.</span></p>
-            <p style={{fontSize: "1.5em"}}>โดยที่ประชาชนแห่งสหประชาชาติได้ยืนยันอีกครั้งไว้ในกฎบัตรถึงศรัทธาในสิทธิมนุษยชนขั้นพื้นฐาน</p>
+            <p className="topic">Tenz Tour's Official Website</p>
+            <p style={{fontSize: "1.5em"}}>Totally not a scam</p>
+            <hr></hr>
+
+            <a href='https://youtu.be/QB7ACr7pUuE?si=G8EdFbi0d6af9_Rx' style={{textDecoration:'none'}}>
+                <div style={{backgroundColor:"gray", width:"300px", marginLeft:'auto', marginRight:'auto', marginTop:"50px", fontSize:"20px", paddingTop:"5px", paddingBottom:"5px", borderRadius: "7px", color:'white'}}>
+                    Click me to get free robux
+                </div>
+            </a>
         </div>
     )
 }
