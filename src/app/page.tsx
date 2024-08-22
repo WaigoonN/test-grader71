d289bdc7cb15eb8c3d18dcacaf46c67e764a5c9b
WaigoonN <cuswaigoon@gmail.com>
2024-08-23 01:33:13 +0700

    Fixed tenz tour

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ import '@/styles/global.css'
 const Home = () => {
     return (
         <div className='width_normal'>
-            <p className="topic">Tenz Tour's Official Website</p>
+            <p className="topic">Tenz Tour&apos;s Official Website</p>
             <p style={{fontSize: "1.5em"}}>Totally not a scam</p>
             <hr></hr>
 
-            <a href='https://youtu.be/QB7ACr7pUuE?si=G8EdFbi0d6af9_Rx' style={{textDecoration:'none'}}>
-                <div style={{backgroundColor:"gray", width:"300px", marginLeft:'auto', marginRight:'auto', marginTop:"50px", fontSize:"20px", paddingTop:"5px", paddingBottom:"5px", borderRadius: "7px", color:'white'}}>
+            <a href='https://youtu.be/QB7ACr7pUuE?si=G8EdFbi0d6af9_Rx' style={{textDecoration:"none"}}>
+                <div style={{backgroundColor:"gray", width:"300px", marginLeft:"auto", marginRight:"auto", marginTop:"50px", fontSize:"20px", paddingTop:"5px", paddingBottom:"5px", borderRadius: "7px", color:"white"}}>
                     Click me to get free robux
                 </div>
             </a>
